57cbadc585389dcab9f1fd801361ed595f9cd800
Taha Tahereddine <tahereddine2002@gmail.com>
2024-12-22 21:48:24 +0100

    Home: Added Component MovieList

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,9 @@
 import React, {useState, useEffect} from "react";
-import { useNavigate } from "react-router-dom";
-import Card from "../components/Card/Card";
+import MoviesList from "../components/MoviesList/MoviesList";
+
 function Home() {
     const [movies, setMovies] = useState([]);
     const [genre, setGenre] = useState([]);
-    const navigate = useNavigate();
 
     useEffect(() => {
         fetch("https://api.themoviedb.org/3/discover/movie?api_key=1de54ccbfea3c2dcfeffd0338867c3b5")
@@ -22,43 +21,9 @@ function Home() {
                 console.log(data.genres);
             });
         }, []);
-    const styles ={
-        moviesList: {
-            marginLeft: "270px",
-            justifyContent: "center",
-            display: "inline-block",
-            marginTop: "80px",
-            backgroundColor: "#060b26"
-        }
-    }
-
-    const handleCardClick = (id) => {
-        navigate(`/movie/${id}`);
-    };
-    
+   
     return (
-        <div className="movies-list" style={styles.moviesList}>
-            <h1 style={{color:" #ffd700", margin:"0 0 20px 30px"}}>Discover</h1>
-            {movies.map((movie) => (
-                <div
-                key={movie.id}
-                onClick={() => handleCardClick(movie.id)}
-                style={{ display: "inline-block", cursor: "pointer" }}
-                >
-                <Card image={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                id={movie.id}
-                title={movie.title}
-                release_date={movie.release_date}
-                genres={genre.map((genre) => {
-                    if(movie.genre_ids.includes(genre.id)){
-                        return genre.name ? genre.name: "";
-                    }
-                })}
-                rating={Number(movie.vote_average).toFixed(1)}
-                ></Card>
-                </div>
-            ))}
-        </div>
+        <MoviesList movies={movies} genre={genre}></MoviesList>
     );
 }
 export default Home;
