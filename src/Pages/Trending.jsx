0e7f9e9a0ee6b6c53f16f21f1d3c467fb5b7ce8f
railus2 <othmanaboutaj@gmail.com>
2024-12-22 21:46:53 +0100

    Update Trending.jsx

--- src/Pages/Trending.jsx
+++ src/Pages/Trending.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Card from '../components/Card/Card';
 import '../components/Card/Card.css';
+import './Trending.css';
 
 function Trending() {
     const [movies, setMovies] = useState(null);
