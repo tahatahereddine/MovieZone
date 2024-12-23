7d524cd9f4997f2814a99a505dc74f94397c9901
ibrahim salah <ibrahimsalahzefri75@gmail.com>
2024-12-23 01:49:45 +0100

    ajout de foctinalite search for movies and tv show

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -3,14 +3,27 @@ import MoviesList from '../../components/MoviesList/MoviesList';
 import { useParams } from 'react-router-dom';
 function Search(){
     const { search } = useParams();
-    const [searchResult, setSearchResult] = useState([]);
+    const [searchResult1, setSearchResult1] = useState([]);
+    const [searchResult2, setSearchResult2] = useState([]);
     const [genre, setGenre] = useState([]);
     
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/search/movie?api_key=1de54ccbfea3c2dcfeffd0338867c3b5&language=en-US&query=${search}`)
             .then(response => response.json())
             .then(data => {
-                setSearchResult(data.results);
+                setSearchResult1(data.results);
+                console.log(data.results);
+                console.log("Search results fetched successfully");
+            })
+            .catch(error => {
+                console.error("Error fetching search results:", error);
+            });
+    }, [search]);
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/search/tv?api_key=1de54ccbfea3c2dcfeffd0338867c3b5&language=en-US&query=${search}`)
+            .then(response => response.json())
+            .then(data => {
+                setSearchResult2(data.results);
                 console.log(data.results);
                 console.log("Search results fetched successfully");
             })
@@ -25,7 +38,7 @@ function Search(){
                     setGenre(data.genres);
                 });
     }, [search]);
-
+    const searchResult = searchResult1.concat(searchResult2);
     return (
         <>
             <MoviesList movies={searchResult} genre={genre} ></MoviesList>
